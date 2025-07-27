dce290b5e7c858eb53c15508d4330699f6f5bf5b
Subodh Shah <shahsubodh166@gmail.com>
2025-07-27 22:39:14 +0545

    complete library without save over refresh

--- main.js
+++ main.js
@@ -1,40 +1,118 @@
+let bookLibrary = [];
 
-let myLibrary = [];
-
-function Book (name, author, pages, status){
+function Book(name, author, pages, status = 'Reading') {
 	this.name = name;
 	this.author = author;
 	this.pages = pages;
 	this.status = status;
 }
 
-Book.prototype.changeStatus = function (status) {
-	let statusChangeBtn = document.getElementByClassName("status-change-btn");
-	statusChangeBtn.addEventListener("click", () => {
-		if (status = "Reading") {
-			status = "Completed";
-		}
-		
-		else{
-			status = "Reading";
-		}
-	});				
-}
-			
-function addBookToLibrary () {
-	let addBookBtn = document.getElementById("addBookBtn");
-	console.log(addBookBtn);
+Book.prototype.changeStatus = function () {
+	this.status = this.status === 'Reading' ? 'Completed' : 'Reading';
+};
+
+
+function addBookToLibrary() {
+	
+	let bookName = document.getElementById('bookName');
+	let bookAuthor = document.getElementById('bookAuthor');
+	let bookPages = document.getElementById('bookPages');
+	let addBookBtn = document.getElementById('addButton');
+	let addBookForm = document.getElementById('addBookForm');
+	let storeBtn = document.getElementById('storeBtn');
+	
 	addBookBtn.addEventListener('click', () => {
-		
-	})
-	let bookName = document.getElementById("getName");
-	let bookAuthor = document.getElementById("getAuthor");
-	let bookPages = document.getElementById("getPages");
-	let bookStatus = document.getElementById("getStatus");
-	let newBook = new Book (bookName, bookAuthor, bookPages, bookStatus);
-	myLibrary.push(newBook);
-}
+		if (addBookForm.hasAttribute('hidden')) {
+			addBookForm.removeAttribute('hidden');
+			addBookBtn.innerText = 'Cancel';
+		}
+		else {
+			addBookBtn.innerText = 'Add Book';
+		    addBookForm.setAttribute('hidden', '');
+			bookName.value = '';
+			bookAuthor.value = '';
+			bookPages.value = '';
+		}
+	});
+	
+	storeBtn.addEventListener('click', () => {
+		let bookStatus = document.querySelector('input[name="bookStatus"]:checked');
+		let getName = bookName.value;
+		let getAuthor = bookAuthor.value;
+		let getPages = Number(bookPages.value);
+		let getStatus = bookStatus.value;
 			
-function bookRenderer (){}
-console.log("HI!");
+		if (typeof(getName) !== 'string' || typeof(getAuthor) !== 'string' || typeof(getPages) !== 'number' || typeof(getStatus) !== 'string') {
+			alert('Enter correct information');
+		} 
+		
+		else if (getName == '' || getAuthor == '' || getPages == '' || getStatus == ''){
+			alert('Plese enter the information');
+		}
+		
+
+		else {
+			let newBook = new Book(getName, getAuthor, getPages, getStatus);
+			bookLibrary.push(newBook);
+			bookRenderer(bookLibrary);
+			changeStatusOfBook();
+			addBookBtn.innerText = 'Add Book';
+			addBookForm.setAttribute('hidden', '');
+			bookName.value = '';
+			bookAuthor.value = '';
+			bookPages.value = '';
+			bookStatus.value = '';
+		}
+	});
+}
+	
+function bookRenderer (bookLibrary) {
+	let booksRecord = document.getElementById('booksRecord');
+	booksRecord.innerHTML = `<tr>
+						<th>Book Name</th>
+						<th>Author</th>
+						<th>Pages</th>
+						<th>Status</th>
+					</tr>`;
+	bookLibrary.forEach((book, index) => {
+		let bookName = book.name;
+		let bookAuthor = book.author;
+		let bookPages = book.pages;
+		let bookStatus = book.status;
+	
+		let addedBook = document.createElement('tr');
+		addedBook.innerHTML = `
+		<tr>
+			<td>${book.name}</td>
+			<td>${book.author}</td>
+			<td>${book.pages}</td>
+			<td>
+				<div class="status-section">
+					<p class="status-text">${book.status}</p>
+					<input type="checkbox" class="status-change-btn" data-index="${index}" ${book.status === 'Completed' ? 'checked' : ''}>
+				</div>
+			</td>
+		</tr>`;
+		booksRecord.appendChild(addedBook);
+	});
+}
+
+function changeStatusOfBook() {
+	let statusChange = [...document.getElementsByClassName('status-change-btn')];
+
+	statusChange.forEach((checkbox) => {
+		checkbox.addEventListener('change', () => {
+			const index = parseInt(checkbox.dataset.index, 10);
+			bookLibrary[index].changeStatus();
+			bookRenderer(bookLibrary);
+			changeStatusOfBook(); // Re-bind listeners after re-rendering
+		});
+	});
+}
+
+
 addBookToLibrary();
+
+
+
+
